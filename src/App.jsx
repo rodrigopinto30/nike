208341eab45119b1e38518ca526601ec81c2e04f
Rodrigo Pinto <rodrigo.pinto@kyas.com.ar>
2023-11-20 20:11:01 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Services, SpecialOffer, CustomerReviews, Footer, Hero, PopularProducts, SuperQuality, Subscribe} from './sections';
+import Nav from './components/Nav';
+
+const App =()=> (
+  <main className="ralative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
