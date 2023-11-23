d19a70c4c5a3a55b9b3af9478f07a364b750fad4
Rodrigo Pinto <rodrigo.pinto@kyas.com.ar>
2023-11-23 19:09:51 -0300

    modificacion de seccion Hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '../components/Button';
 import {arrowRight} from "../assets/icons";
+import { statistics } from '../constants';
+import { bigShoe1 } from '../assets/images';
 
 const Hero = () => {
   return (
@@ -12,19 +14,44 @@ const Hero = () => {
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full 
         max-xl:padding-x pt-28"
       >
-        <p> Our Summer collections</p>
-        <h1> 
-          <span> The New Arrival</span>
-          <brr/>
-          <span>Nike</span>
-          Shoes
+        <p className='text-xl font-monserrat text-coral-red'> Our Summer collections</p>
+        <h1 className='mt-10 font-palanquin text-8xl 
+          max-sm:text-[72px]
+          max-sm:leading-[82] font-bold'
+        > 
+          <span className='xl:bg-white 
+            xl:whitespace-nowrap relative z-10 pr-10'
+          > The New Arrival</span>
+          <br/>
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
-        <p> Discover stylish Nike arrivals, quality comfort, and 
+        <p className='font-montserrat text-slate-gray text-lg leading-8
+          mt-6 mb-14 
+          sm:max-w-sm'
+        > Discover stylish Nike arrivals, quality comfort, and 
           innovation for your active life.
         </p>
         <Button 
           label="Shop now"
           iconURL={arrowRight}
+        />
+        <div className='flex justify-starts items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((stat, index)=>(
+            <div key={index}>
+              <p className='text-4xl font-palaquin font-bold'> {stat.value} </p>
+              <p className='leading-7 font-montserrat text-slate-gray'> {stat.label} </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='relative flex-1 flex justify-center items-center
+        xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'
+      >
+        <img 
+          src={bigShoe1} 
+          alt="Shoe collection"
+          className='object-contain relative z-10'
         />
       </div>
     </section>
