bf69d374ee28a0992f96bf504b304ac5a0221571
Rodrigo Pinto <rodrigo.pinto@kyas.com.ar>
2023-11-24 19:01:45 -0300

    Incorporacion de ShoeCard

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Button from '../components/Button';
 import {arrowRight} from "../assets/icons";
-import { statistics } from '../constants';
+import { shoes, statistics } from '../constants';
 import { bigShoe1 } from '../assets/images';
+import {ShoeCard} from '../components/ShoeCard';
 
 const Hero = () => {
+
+  const [bigShoeImg, setBigShoeImg] = React.useState(bigShoe1);
+
   return (
     <section 
       id='home'
@@ -49,10 +53,22 @@ const Hero = () => {
         xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'
       >
         <img 
-          src={bigShoe1} 
+          src={bigShoeImg} 
           alt="Shoe collection"
           className='object-contain relative z-10'
         />
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((shoe, index) =>(
+            <div key={index}>
+              <ShoeCard 
+                index={index}
+                imgURL={shoe}
+                changeBigShoeImage={(shoe)=>{setBigShoeImg(shoe)}}
+                bihShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
